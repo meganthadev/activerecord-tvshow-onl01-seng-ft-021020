5b138f0d430a37df8106c521be12668b72662d29
Megan Horsfield <meganthadev@gmail.com>
2020-04-02 04:13:15 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,7 +2,7 @@ class CreateShows < Active Record::Migration[4.2]
   def change
     create_table :shows do |t|
       t.string :name
-      t.integer :price
+      t.integer :network
       t.string :size  
       t.string :image_url
       t.timestamps 
