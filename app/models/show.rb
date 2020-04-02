b1acb4c4293ae5055c8c295c339cf139fa55acbc
Megan Horsfield <meganthadev@gmail.com>
2020-04-02 04:26:43 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,6 +9,25 @@ class Show < ActiveRecord::Base
   end 
   
   def lowest_rating
+    
+  end 
+  
+  def least_popular_show 
+    
+  end 
+  
+  def ratings_sum 
+    
+  end 
+  
+  def popular_shows
+    
+  end 
+  
+  def shows_by_alphabetical_order
+    
+  end 
+
   
   
   
